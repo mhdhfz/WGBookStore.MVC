abcb61702c74f4dc4bbbb12a9abde8e7fb690c78
mhdhfz <mhd.hfzz@gmail.com>
2021-09-03 08:55:36 +0800

    add controllerswithviews method at configureService, change the endpoint to mapdefaultcontrollerRoute and create home controller and index action

---
+++ WGBookStore.MVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGBookStore.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+		{
+			return "WGBookStore";
+		}
+    }
+}
