f7006435747941234acc6c37bdcc8a26b4231dad
mhdhfz <mhd.hfzz@gmail.com>
2021-09-04 07:08:04 +0800

    setup dbcontext, add connection string

---
+++ WGBookStore.MVC/Data/BookStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGBookStore.MVC.Data
+{
+    public class BookStoreContext : DbContext
+    {
+		public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Book> Books { get; set; }
+	}
+}
--- WGBookStore.MVC/Startup.cs
+++ WGBookStore.MVC/Startup.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WGBookStore.MVC.Data;
 using WGBookStore.MVC.Interfaces;
 using WGBookStore.MVC.Repositories;
 
@@ -14,10 +17,19 @@ namespace WGBookStore.MVC
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<BookStoreContext>(options =>
+			   options.UseSqlServer(Configuration.GetConnectionString("BookStoreConnection")));
 			services.AddControllersWithViews();
 			services.AddScoped<IBookRepository, BookRepository>();
 		}
