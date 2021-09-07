33b8e68cd488fdfad39944fd0ec99ac57b82f50c
mhdhfz <mhd.hfzz@gmail.com>
2021-09-07 08:09:58 +0800

    configure password complexity

--- WGBookStore.MVC/Startup.cs
+++ WGBookStore.MVC/Startup.cs
@@ -34,6 +34,16 @@ namespace WGBookStore.MVC
 			   options.UseSqlServer(Configuration.GetConnectionString("BookStoreConnection")));
 			services.AddIdentity<ApplicationUser, IdentityRole>()
 				.AddEntityFrameworkStores<BookStoreContext>();
+
+			services.Configure<IdentityOptions>(options =>
+			{
+				options.Password.RequiredLength = 5;
+				options.Password.RequiredUniqueChars = 1;
+				options.Password.RequireDigit = false;
+				options.Password.RequireLowercase = false;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireUppercase = false;
+			});
 			services.AddControllersWithViews();
 			services.AddScoped<IBookRepository, BookRepository>();
 			services.AddScoped<ILanguageRepository, LanguageRepository>();
